6dcd60ff623748dda2925daef14e1e7b7ec5e5c5
Yurii Voskoboinikov <voscob@gmail.com>
2022-10-07 20:35:22 +0300

    Add a destination integration test

---
+++ destination/destination_integration_test.go
@@ -0,0 +1,339 @@
+// Copyright © 2022 Meroxa, Inc. & Yalantis
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package destination
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/conduitio-labs/conduit-connector-clickhouse/config"
+	sdk "github.com/conduitio/conduit-connector-sdk"
+	"github.com/jmoiron/sqlx"
+	"github.com/matryer/is"
+)
+
+func TestDestination_Write(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ctx = context.Background()
+		cfg = prepareConfig(t)
+		is  = is.New(t)
+	)
+
+	db, err := sqlx.Open("clickhouse", cfg[config.URL])
+	is.NoErr(err)
+	defer db.Close()
+
+	err = db.Ping()
+	is.NoErr(err)
+
+	err = createTable(db, cfg[config.Table])
+	is.NoErr(err)
+
+	defer func() {
+		err = dropTable(db, cfg[config.Table])
+		is.NoErr(err)
+	}()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	dest := NewDestination()
+
+	err = dest.Configure(ctx, cfg)
+	is.NoErr(err)
+
+	err = dest.Open(ctx)
+	is.NoErr(err)
+
+	records := []sdk.Record{
+		{
+			Operation: sdk.OperationSnapshot,
+			Payload: sdk.Change{After: sdk.StructuredData{
+				"int_type":    42,
+				"string_type": "John",
+			}},
+		},
+		{
+			Operation: sdk.OperationSnapshot,
+			Payload: sdk.Change{After: sdk.StructuredData{
+				"int_type":    43,
+				"string_type": "Nick",
+			}},
+		},
+	}
+
+	n, err := dest.Write(ctx, records)
+	is.NoErr(err)
+	is.Equal(n, len(records))
+
+	cancel()
+
+	err = dest.Teardown(context.Background())
+	is.NoErr(err)
+}
+
+func TestDestination_Write_Update(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ctx = context.Background()
+		cfg = prepareConfig(t)
+		is  = is.New(t)
+	)
+
+	db, err := sqlx.Open("clickhouse", cfg[config.URL])
+	is.NoErr(err)
+	defer db.Close()
+
+	err = db.Ping()
+	is.NoErr(err)
+
+	err = createTable(db, cfg[config.Table])
+	is.NoErr(err)
+
+	defer func() {
+		err = dropTable(db, cfg[config.Table])
+		is.NoErr(err)
+	}()
+
+	err = insertData(db, cfg[config.Table])
+	is.NoErr(err)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	dest := NewDestination()
+
+	err = dest.Configure(ctx, cfg)
+	is.NoErr(err)
+
+	err = dest.Open(ctx)
+	is.NoErr(err)
+
+	n, err := dest.Write(ctx, []sdk.Record{
+		{
+			Operation: sdk.OperationUpdate,
+			Key: sdk.StructuredData{
+				"int_type": 42,
+			},
+			Payload: sdk.Change{After: sdk.StructuredData{
+				"int_type":    42,
+				"string_type": "Jane",
+			}},
+		},
+	})
+	is.NoErr(err)
+	is.Equal(n, 1)
+
+	name, err := getStringFieldByIntField(db, cfg[config.Table], 42)
+	is.NoErr(err)
+	is.Equal(name, "Jane")
+
+	cancel()
+
+	err = dest.Teardown(context.Background())
+	is.NoErr(err)
+}
+
+func TestDestination_Write_Delete(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ctx = context.Background()
+		cfg = prepareConfig(t)
+		is  = is.New(t)
+	)
+
+	db, err := sqlx.Open("clickhouse", cfg[config.URL])
+	is.NoErr(err)
+	defer db.Close()
+
+	err = db.Ping()
+	is.NoErr(err)
+
+	err = createTable(db, cfg[config.Table])
+	is.NoErr(err)
+
+	defer func() {
+		err = dropTable(db, cfg[config.Table])
+		is.NoErr(err)
+	}()
+
+	err = insertData(db, cfg[config.Table])
+	is.NoErr(err)
+
+	// check if row exists
+	_, err = getStringFieldByIntField(db, cfg[config.Table], 42)
+	is.NoErr(err)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	dest := NewDestination()
+
+	err = dest.Configure(ctx, cfg)
+	is.NoErr(err)
+
+	err = dest.Open(ctx)
+	is.NoErr(err)
+
+	n, err := dest.Write(ctx, []sdk.Record{
+		{
+			Operation: sdk.OperationDelete,
+			Key:       sdk.RawData(`{"int_type":42}`),
+		},
+	})
+	is.NoErr(err)
+	is.Equal(n, 1)
+
+	_, err = getStringFieldByIntField(db, cfg[config.Table], 42)
+	is.True(err != nil)
+
+	cancel()
+
+	err = dest.Teardown(context.Background())
+	is.NoErr(err)
+}
+
+func TestDestination_Write_WrongColumn(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ctx = context.Background()
+		cfg = prepareConfig(t)
+		is  = is.New(t)
+	)
+
+	db, err := sqlx.Open("clickhouse", cfg[config.URL])
+	is.NoErr(err)
+	defer db.Close()
+
+	err = db.Ping()
+	is.NoErr(err)
+
+	err = createTable(db, cfg[config.Table])
+	is.NoErr(err)
+
+	defer func() {
+		err = dropTable(db, cfg[config.Table])
+		is.NoErr(err)
+	}()
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	dest := NewDestination()
+
+	err = dest.Configure(ctx, cfg)
+	is.NoErr(err)
+
+	err = dest.Open(ctx)
+	is.NoErr(err)
+
+	_, err = dest.Write(ctx, []sdk.Record{
+		{
+			Operation: sdk.OperationSnapshot,
+			Payload: sdk.Change{After: sdk.StructuredData{
+				"int_type":     43,
+				"wrong_column": "test",
+			}},
+		},
+	})
+	is.True(err != nil)
+
+	cancel()
+
+	err = dest.Teardown(context.Background())
+	is.NoErr(err)
+}
+
+func prepareConfig(t *testing.T) map[string]string {
+	url := os.Getenv("CLICKHOUSE_URL")
+	if url == "" {
+		t.Skip("CLICKHOUSE_URL env var must be set")
+
+		return nil
+	}
+
+	return map[string]string{
+		config.URL:   url,
+		config.Table: fmt.Sprintf("CONDUIT_DEST_TEST_%s", randString(6)),
+	}
+}
+
+func createTable(db *sqlx.DB, table string) error {
+	_, err := db.Exec(fmt.Sprintf(`
+	CREATE TABLE %s
+(
+    int_type    Int32,
+    string_type String,
+) ENGINE ReplacingMergeTree() PRIMARY KEY int_type;`, table))
+	if err != nil {
+		return fmt.Errorf("execute create table query: %w", err)
+	}
+
+	return nil
+}
+
+func dropTable(db *sqlx.DB, table string) error {
+	_, err := db.Exec(fmt.Sprintf("DROP TABLE %s", table))
+	if err != nil {
+		return fmt.Errorf("execute drop table query: %w", err)
+	}
+
+	return nil
+}
+
+func insertData(db *sqlx.DB, table string) error {
+	_, err := db.Exec(fmt.Sprintf("INSERT INTO %s (int_type, string_type) VALUES (42, 'Sam');", table))
+	if err != nil {
+		return fmt.Errorf("execute insert query: %w", err)
+	}
+
+	return nil
+}
+
+func getStringFieldByIntField(db *sqlx.DB, table string, id int) (string, error) {
+	// wait a bit
+	time.Sleep(time.Second)
+
+	row := db.QueryRow(fmt.Sprintf("SELECT string_type FROM %s WHERE int_type = %d;", table, id))
+
+	name := ""
+
+	err := row.Scan(&name)
+	if err != nil {
+		return "", fmt.Errorf("scan row: %w", err)
+	}
+
+	return name, nil
+}
+
+// randString generates a random string of length n.
+// (source: https://stackoverflow.com/a/47676287)
+func randString(n int) string {
+	b := make([]byte, n)
+	rand.Read(b) //nolint:errcheck // does not actually fail
+
+	return strings.ToUpper(hex.EncodeToString(b))
+}
