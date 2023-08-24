83ad1c71e7d9e999411d12ec811d6dc230dfd755
maha-hajja <maha.d.hajja@gmail.com>
2023-08-24 13:48:22 +0100

    add timeout to golangci-lint

--- spec.go
+++ spec.go
@@ -19,9 +19,8 @@ import (
 )
 
 // version is set during the build process (i.e. the Makefile).
-// It follows Go's convention for module version, where the version
-// starts with the letter v, followed by a semantic version.
-var version = "v0.0.0-dev"
+// Default version matches default from runtime/debug.
+var version = "(devel)"
 
 // Specification returns specification of the connector.
 func Specification() sdk.Specification {
