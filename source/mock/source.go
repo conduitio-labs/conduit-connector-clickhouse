1070bbc577a3518f901ebce0829de709e23d73dc
Lovro Mažgon <lovro.mazgon@gmail.com>
2025-04-01 18:58:16 +0200

    make generate

--- source/mock/source.go
+++ source/mock/source.go
@@ -21,6 +21,7 @@ import (
 type MockIterator struct {
 	ctrl     *gomock.Controller
 	recorder *MockIteratorMockRecorder
+	isgomock struct{}
 }
 
 // MockIteratorMockRecorder is the mock recorder for MockIterator.
